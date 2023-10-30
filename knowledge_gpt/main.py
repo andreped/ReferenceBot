b70e387cc0f51f100829cdf24b40c8fd9c21146f
André Pedersen <andrped94@gmail.com>
2023-10-30 14:40:33 +0100

    Refactored main

--- knowledge_gpt/main.py
+++ knowledge_gpt/main.py
@@ -48,14 +48,6 @@ def main():
 
     sidebar()
 
-    openai_api_key = os.environ["OPENAI_API_KEY"]
-
-    if not openai_api_key:
-        st.warning(
-            "Enter your OpenAI API key in the sidebar. You can get a key at"
-            " https://platform.openai.com/account/api-keys."
-        )
-
     uploaded_file = st.file_uploader(
         "Upload a pdf, docx, or txt file",
         type=["pdf", "docx", "txt"],
