1e530204cc8004fde830cd7804cc117deb48ad20
André Pedersen <andrped94@gmail.com>
2023-10-30 10:20:34 +0100

    Added KnowledgeGPT code to project

---
+++ knowledge_gpt/components/sidebar.py
@@ -0,0 +1,43 @@
+import streamlit as st
+
+from knowledge_gpt.components.faq import faq
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+
+def sidebar():
+    with st.sidebar:
+        st.markdown(
+            "## How to use\n"
+            "1. Enter your [OpenAI API key](https://platform.openai.com/account/api-keys) below🔑\n"  # noqa: E501
+            "2. Upload a pdf, docx, or txt file📄\n"
+            "3. Ask a question about the document💬\n"
+        )
+        api_key_input = st.text_input(
+            "OpenAI API Key",
+            type="password",
+            placeholder="Paste your OpenAI API key here (sk-...)",
+            help="You can get your API key from https://platform.openai.com/account/api-keys.",  # noqa: E501
+            value=os.environ.get("OPENAI_API_KEY", None)
+            or st.session_state.get("OPENAI_API_KEY", ""),
+        )
+
+        st.session_state["OPENAI_API_KEY"] = api_key_input
+
+        st.markdown("---")
+        st.markdown("# About")
+        st.markdown(
+            "📖KnowledgeGPT allows you to ask questions about your "
+            "documents and get accurate answers with instant citations. "
+        )
+        st.markdown(
+            "This tool is a work in progress. "
+            "You can contribute to the project on [GitHub](https://github.com/mmz-001/knowledge_gpt) "  # noqa: E501
+            "with your feedback and suggestions💡"
+        )
+        st.markdown("Made by [mmz_001](https://twitter.com/mm_sasmitha)")
+        st.markdown("---")
+
+        faq()
---
+++ knowledge_gpt/main.py
@@ -0,0 +1,121 @@
+import streamlit as st
+
+from knowledge_gpt.components.sidebar import sidebar
+
+from knowledge_gpt.ui import (
+    wrap_doc_in_html,
+    is_query_valid,
+    is_file_valid,
+    is_open_ai_key_valid,
+    display_file_read_error,
+)
+
+from knowledge_gpt.core.caching import bootstrap_caching
+
+from knowledge_gpt.core.parsing import read_file
+from knowledge_gpt.core.chunking import chunk_file
+from knowledge_gpt.core.embedding import embed_files
+from knowledge_gpt.core.qa import query_folder
+from knowledge_gpt.core.utils import get_llm
+
+
+EMBEDDING = "openai"
+VECTOR_STORE = "faiss"
+MODEL_LIST = ["gpt-3.5-turbo", "gpt-4"]
+
+# Uncomment to enable debug mode
+# MODEL_LIST.insert(0, "debug")
+
+st.set_page_config(page_title="KnowledgeGPT", page_icon="📖", layout="wide")
+st.header("📖KnowledgeGPT")
+
+# Enable caching for expensive functions
+bootstrap_caching()
+
+sidebar()
+
+openai_api_key = st.session_state.get("OPENAI_API_KEY")
+
+
+if not openai_api_key:
+    st.warning(
+        "Enter your OpenAI API key in the sidebar. You can get a key at"
+        " https://platform.openai.com/account/api-keys."
+    )
+
+
+uploaded_file = st.file_uploader(
+    "Upload a pdf, docx, or txt file",
+    type=["pdf", "docx", "txt"],
+    help="Scanned documents are not supported yet!",
+)
+
+model: str = st.selectbox("Model", options=MODEL_LIST)  # type: ignore
+
+with st.expander("Advanced Options"):
+    return_all_chunks = st.checkbox("Show all chunks retrieved from vector search")
+    show_full_doc = st.checkbox("Show parsed contents of the document")
+
+
+if not uploaded_file:
+    st.stop()
+
+try:
+    file = read_file(uploaded_file)
+except Exception as e:
+    display_file_read_error(e, file_name=uploaded_file.name)
+
+chunked_file = chunk_file(file, chunk_size=300, chunk_overlap=0)
+
+if not is_file_valid(file):
+    st.stop()
+
+
+if not is_open_ai_key_valid(openai_api_key, model):
+    st.stop()
+
+
+with st.spinner("Indexing document... This may take a while⏳"):
+    folder_index = embed_files(
+        files=[chunked_file],
+        embedding=EMBEDDING if model != "debug" else "debug",
+        vector_store=VECTOR_STORE if model != "debug" else "debug",
+        openai_api_key=openai_api_key,
+    )
+
+with st.form(key="qa_form"):
+    query = st.text_area("Ask a question about the document")
+    submit = st.form_submit_button("Submit")
+
+
+if show_full_doc:
+    with st.expander("Document"):
+        # Hack to get around st.markdown rendering LaTeX
+        st.markdown(f"<p>{wrap_doc_in_html(file.docs)}</p>", unsafe_allow_html=True)
+
+
+if submit:
+    if not is_query_valid(query):
+        st.stop()
+
+    # Output Columns
+    answer_col, sources_col = st.columns(2)
+
+    llm = get_llm(model=model, openai_api_key=openai_api_key, temperature=0)
+    result = query_folder(
+        folder_index=folder_index,
+        query=query,
+        return_all=return_all_chunks,
+        llm=llm,
+    )
+
+    with answer_col:
+        st.markdown("#### Answer")
+        st.markdown(result.answer)
+
+    with sources_col:
+        st.markdown("#### Sources")
+        for source in result.sources:
+            st.markdown(source.page_content)
+            st.markdown(source.metadata["source"])
+            st.markdown("---")
